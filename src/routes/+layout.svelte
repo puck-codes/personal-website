e9764741229a9c3e165c006c5b1a938f1454e57d
Puck <brayansc1119@gmail.com>
2024-01-23 14:34:05 +0100

    add css-normalizer, sass dev and svelte preprocessor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import "modern-normalize/modern-normalize.css"
 </script>
 
 <div class="app">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,3 +9,12 @@
 <section>
 	<h1>1</h1>
 </section>
+
+<style lang="scss">
+
+	h1 {
+		color: red;
+		font-size: toRem(36);
+	}
+
+</style>
